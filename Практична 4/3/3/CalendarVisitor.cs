6f4c99cbf9743b03729eea625574949c2d775aff
freshusername <40106176+freshusername@users.noreply.github.com>
2019-03-27 11:51:34 +0200

    ..

--- Практична 4/3/3/CalendarVisitor.cs
+++ Практична 4/3/3/CalendarVisitor.cs
@@ -8,9 +8,12 @@ namespace _3
 {
     class CalendarVisitor : AbstractVisitor
     {
-        public override void VisitUser1(AbstractElement element)
+        public override string VisitUser1(AbstractElement element)
         {
-            
+            string s;
+            User1 user1 = element as User1;
+            user1.IsTaken = true;
+            return s = $"User {user1.Name} set new narada {user1.Narada} in: ....";
         }
     }
 }
--- Практична 4/3/3/User1.cs
+++ Практична 4/3/3/User1.cs
@@ -8,6 +8,16 @@ namespace _3
 {
     class User1 : AbstractElement
     {
+        public bool IsTaken { get; set; }
+        public string Name { get; set; }
+        public string Narada { get; set; }
+        public User1(string name, string narada, bool istaken)
+        {
+            this.Name = name;
+            this.Narada = narada;
+            this.IsTaken = istaken;
+        }
+
         public override string Accept(AbstractVisitor visitor)
         {
             visitor.VisitUser1(this);
