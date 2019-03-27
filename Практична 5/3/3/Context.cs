cd19c15b218b7c9de9f5442e65a56a29605eea90
freshusername <40106176+freshusername@users.noreply.github.com>
2019-03-27 17:02:32 +0200

    3 from 5pr

---
+++ Практична 5/3/3/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Context
+    {
+        AbstractPrinterStrategy abstractPrinter;
+        public Context(AbstractPrinterStrategy abstractPrinter)
+        {
+            this.abstractPrinter = abstractPrinter;
+        }
+        public string ExcecuteAlgoritm()
+        {
+            return this.abstractPrinter.Algoritm();
+        }
+    }
+}
---
+++ Практична 5/3/3/LedPrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class LedPrinter : AbstractPrinterStrategy
+    {
+        public override string Algoritm()
+        {
+            string src;
+            return "Led printer prints led pages: ";
+        }
+    }
+}
